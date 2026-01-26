f9adfc347a1a800ec07b169a9e016963ed7e2d89
Ezequiel <ezql.lima@gmail.com>
2026-01-26 11:48:07 -0300

    Python

---
+++ estudos-python/curso_python/aula1.py
@@ -0,0 +1,11 @@
+"""
+Comentário docstring, essas linhas são lidas pelo interpretador do python também
+
+três aspas para abrir e três para fechar
+ISSO NÃO É COMENTÁRIO, É CHAMADO DE DOCSTRING
+
+"""
+
+
+# Comentário de código em python
+print(123)
