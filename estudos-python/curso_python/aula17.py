13f1b0c8b1a52bc07f9819de00dbfe058630a620
Ezequiel <ezql.lima@gmail.com>
2026-01-26 23:34:14 -0300

    Remove unused portfolio folders (PORTFOLIO-PRO, PORTFOLIOOPUS, PROJETOPORTFLIO)

---
+++ estudos-python/curso_python/aula17.py
@@ -0,0 +1,17 @@
+condicao1 = True
+condicao2 = False
+condicao3 = False
+condicao4 = False
+
+if condicao1:
+    print('Codigo da condicao 1')
+elif condicao2:
+    print('Codigo da condicao 2')
+elif condicao3:
+    print('Codigo da condicao 3')
+elif condicao4:
+    print('Codigo da condicao 4')
+else:
+    print('------')
+    
+print('Fora do if')
